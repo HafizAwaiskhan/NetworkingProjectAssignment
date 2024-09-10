21bdbe66cea8cf6757d1f8e2b2e2fb70e0a990ff
hafizawaiskhan <hafizawaiskhan1992@gmail.com>
2024-09-11 03:49:37 +0500

    Upgrade version 1.7.0

--- retrofitLib/src/main/java/com/example/retrofitlib/NetworkingLibrary.kt
+++ retrofitLib/src/main/java/com/example/retrofitlib/NetworkingLibrary.kt
@@ -1,9 +1,6 @@
 package com.example.retrofitlib
 
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.HttpException
-import retrofit2.Response
 
 object NetworkingLibrary {
 
@@ -13,30 +10,21 @@ object NetworkingLibrary {
         language: String = "en-US",
         page: Int = 1,
         baseUrl: String,
-        token: String? = null,
-        callback: (Result<ApiResponse<T>>) -> Unit
-    ) {
+        token: String? = null
+    ): Result<ApiResponse<T>> {
         val apiService = NetworkModule.createService(ApiService::class.java, baseUrl, token)
-        val call = apiService.search<T>(query, includeAdult, language, page)
-
-        call.enqueue(object : Callback<ApiResponse<T>> {
-            override fun onResponse(
-                call: Call<ApiResponse<T>>,
-                response: Response<ApiResponse<T>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        callback(Result.Success(it))
-                    } ?: callback(Result.Error(NullPointerException("Response body is null")))
-                } else {
-                    callback(Result.Error(HttpException(response)))
-                }
+        return try {
+            val response = apiService.search<T>(query, includeAdult, language, page).execute()
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    Result.Success(it)
+                } ?: Result.Error(NullPointerException("Response body is null"))
+            } else {
+                Result.Error(HttpException(response))
             }
-
-            override fun onFailure(call: Call<ApiResponse<T>>, t: Throwable) {
-                callback(Result.Error(t))
-            }
-        })
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
     }
 }
 
